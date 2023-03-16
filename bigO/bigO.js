2b50366cdca866517113af7943b7ea430a7612d7
YunL97 <leeys9932@naver.com>
2023-03-17 00:39:36 +0900

    알고리즘강의 7강까지

--- bigO/bigO.js
+++ bigO/bigO.js
@@ -2,16 +2,16 @@
 빅오표기법: 코드의 성능을 얘기할때 정확한 전문용어를 사용하는것이 중요한데 알고리즘을 짤때 드는 시간을 말함.  
 */
 
-function addUpTo(n){
-    let total = 0;
-    for(let i = 1; i<=n; i++){
+function addUpTo(n){//5n + 2, 2는 
+    let total = 0; //+1
+    for(let i = 1 /*+1*/; i<=n; i++){  
         total = total + i;
     }
     return total;
 }
 
-function addUpto2(n){
-    return n * (n + 1) / 2
+function addUpto2(n){//3개
+    return n * (n + 1) / 2 
 }
 
 //
@@ -19,3 +19,5 @@ let t1 = performance.now();//브라우저가 이 문서를 만든시간을 알�
 console.log(addUpto2(1000000000))
 let t2 = performance.now();
 console.log(t2 - t1)
+
+//시간은 항상 연산의 갯수에 달려있음 ex) + - x
